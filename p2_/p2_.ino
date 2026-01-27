6bef206230d053b9317ef611b6ca771b3fe29785
Marianne Félix <mariannefelixs11@gmail.com>
2026-01-27 16:05:03 -0300

    feat:  test component display 7 segments

--- p2_/p2_.ino
+++ p2_/p2_.ino
@@ -14,6 +14,31 @@ const int motor_pin = 14;
 #define F 19
 #define G 21
 
+bool sete_segmentos[10][7] = { 
+  { 1,1,1,1,1,1,0 }, // = Digito 0
+  { 0,1,1,0,0,0,0 }, // = Digito 1
+  { 1,1,0,1,1,0,1 }, // = Digito 2
+  { 1,1,1,1,0,0,1 }, // = Digito 3
+  { 0,1,1,0,0,1,1 }, // = Digito 4
+  { 1,0,1,1,0,1,1 }, // = Digito 5
+  { 1,0,1,1,1,1,1 }, // = Digito 6
+  { 1,1,1,0,0,0,0 }, // = Digito 7
+  { 1,1,1,1,1,1,1 }, // = Digito 8
+  { 1,1,1,1,0,1,1 }, // = Digito 9
+};
+
+void escreverNumero(int numero) {
+  bool* display = sete_segmentos[numero];
+  digitalWrite(A,display[0]);
+  digitalWrite(B,display[1]);
+  digitalWrite(C,display[2]);
+  digitalWrite(D,display[3]);
+  digitalWrite(E,display[4]);
+  digitalWrite(F,display[5]);
+  digitalWrite(G,display[6]);
+
+}
+
 
 void test_button_with_led(){
 
@@ -45,12 +70,31 @@ void buzzer_function(int pin, int frequency, int duration, int m_pause){
   
 }
 
+void test_display_7_s(){
+  
+  for(int i = 0; i < 10; i++)
+  {
+    Serial.println(i);
+    escreverNumero(i);
+    delay(1000);
+  }
+
+}
+
 
 
 void setup() {
 
   pinMode(led_pin, OUTPUT);
   pinMode(button_pin, INPUT_PULLUP);
+
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
 
 
   Serial.begin(115200);
@@ -59,6 +103,6 @@ void setup() {
 
 void loop() {
   
-   buzzer_function(buzzer_pin, NOTE_C7, 250, 1);
+  
 
 }
