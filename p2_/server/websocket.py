021d7e97da10014f632f087542b666d8eeeb1a14
mariannef14 <mariannefelixs11@gmail.com>
2026-01-30 00:16:23 -0300

    docs: add comments

--- p2_/server/websocket.py
+++ p2_/server/websocket.py
@@ -4,27 +4,28 @@ import time
 
 import json
 
+#Access file with logins
 with open("server/logins.json", "r") as file:
      users:dict = json.load(file)
 
-
+#Configure server connection
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hostname = socket. gethostname()
-local_ip = socket. gethostbyname(hostname)
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
 print(local_ip)
 
 try:
-    mysock.bind(('',80))
+    mysock.bind((local_ip,80)) #Associates a created socket with a specific IP address and port number on the local host
     
 except socket.error:
     print("Failed to bind")
     sys.exit()
     
-mysock.listen()
+mysock.listen() #Listen to requests
 
 while True:
     conn,addr = mysock.accept()
-    data = conn.recv(1000)
+    data = conn.recv(1000) #Receive requests
 
     time.sleep(3)
     
@@ -32,6 +33,8 @@ while True:
         break
     
     print("Got a request!!!")
+    
+    #Captures information sent by the customer
     data = str(data,'UTF-8')
     
     if data.find("GET") != -1:
@@ -50,7 +53,9 @@ while True:
              message = ""
           
     
+    #Sends a response to the customer
     conn.sendall(bytes(message,"utf-8"))
-    
+
+#Close the connection.    
 conn.close()
 mysock.close()
